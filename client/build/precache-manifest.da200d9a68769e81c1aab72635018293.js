3880f6d0ca1f928e0e5bd68f33070edeb4eaff34
mnkyhdx <mnkyhdx@dhcp-v067-061.mobile.uci.edu>
2019-01-24 20:30:12 -0800

    Heroku Deployment

---
+++ client/build/precache-manifest.da200d9a68769e81c1aab72635018293.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e3349b6800b8bbd3ae9",
+    "url": "/static/js/main.3e3349b6.chunk.js"
+  },
+  {
+    "revision": "9057d39878ab7fa3c04b",
+    "url": "/static/js/1.9057d398.chunk.js"
+  },
+  {
+    "revision": "3e3349b6800b8bbd3ae9",
+    "url": "/static/css/main.e3a2f54a.chunk.css"
+  },
+  {
+    "revision": "e2019e853c212fd11fdcdaab83ab7b44",
+    "url": "/index.html"
+  }
+];
